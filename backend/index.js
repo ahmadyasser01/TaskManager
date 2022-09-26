6d093f6702cf0c6539ad9a50ebdce00c942f0845
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-09-26 11:30:59 +0200

    init server setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,32 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import connectDB from "./db/connect.js";
+
+// CONSTANTS
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+const DBURI = process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017';
+const app = express();
+
+//CORS  OPTIONS
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+}
+
+// MIDDLEWARES
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+//ROUTES
+
+
+
+// SERVER
+connectDB(DBURI);
+app.listen(PORT,()=>{
+    console.log("listening on port",PORT);
+})
