858aef66d382f85226cf969f432d610254fc4446
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-09-29 12:01:31 +0200

    add nav bar

--- frontend/src/components/Nav/Navbar.js
+++ frontend/src/components/Nav/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './navbar.css'
+import {Button} from '@mui/material'
+import API from '../../utils/API'
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  let navigate = useNavigate();
+  const HandleLogout = async ()=>{
+    const {status} =await API.logout();
+    console.log('====================================');
+    console.log();
+    console.log('====================================');
+    if(status === 'Success'){
+      navigate('/')
+    }
+  }
   return (
     <div className='navbar'>
-      nav bar
+      <h1>Task Manager</h1>
+      <Button onClick={HandleLogout}>Logout</Button>
     </div>
   )
 }
